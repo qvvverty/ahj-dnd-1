16641a6aacfe3c81d5ff483e2a6f64b48ec9212b
Leonid Kostyuchenko <kstchnko@gmail.com>
2020-12-13 19:19:38 +0300

    add AddTask module

---
+++ src/js/AddTask.js
@@ -0,0 +1,42 @@
+export default class AddTask {
+  bindToDOM(parentEl) {
+    this.TaskBoardEl = parentEl.querySelector('.taskboard');
+    this.TaskBoardEl.addEventListener('click', this.taskBtnsHandler.bind(this));
+    this.TaskBoardEl.addEventListener('keydown', this.addTask.bind(this));
+  }
+
+  taskBtnsHandler(click) {
+    if (click.target.classList.contains('add-task-btn')) {
+      this.closeAddTaskPopup();
+      this.activeNewTaskEl = click.target.querySelector('.new-task-text-wrapper');
+      this.activeNewTaskTextarea = this.activeNewTaskEl.querySelector('.new-task-text');
+      this.activeNewTaskTextarea.select();
+      this.activeNewTaskEl.classList.add('visible');
+    } else if (!click.target.classList.contains('new-task-text')) {
+      this.closeAddTaskPopup();
+    }
+  }
+
+  addTask(keydown) {
+    if (keydown.keyCode === 13 && keydown.target.classList.contains('new-task-text')) {
+      if (this.activeNewTaskTextarea.value) {
+        const newTaskEl = document.createElement('div');
+        newTaskEl.classList.add('task');
+        newTaskEl.innerText = this.activeNewTaskTextarea.value;
+        this.activeNewTaskEl.closest('.add-task-btn').before(newTaskEl);
+
+        this.closeAddTaskPopup();
+      } else {
+        keydown.preventDefault();
+      }
+    }
+  }
+
+  closeAddTaskPopup() {
+    if (this.activeNewTaskEl) {
+      this.activeNewTaskTextarea.value = '';
+      this.activeNewTaskEl.classList.remove('visible');
+      this.activeNewTaskEl = null;
+    }
+  }
+}
---
+++ src/js/DragAndDrop.js
@@ -0,0 +1,84 @@
+export default class TaskBoard {
+  bindToDOM(parentEl) {
+    this.grab = this.grab.bind(this);
+    this.drag = this.drag.bind(this);
+    this.drop = this.drop.bind(this);
+
+    this.parentEl = parentEl;
+
+    this.taskBoardEl = this.parentEl.querySelector('.taskboard');
+    this.grabbedEl = null;
+
+    this.taskBoardEl.addEventListener('mousedown', this.grab);
+  }
+
+  grab(event) {
+    if (event.target.classList.contains('task')) {
+      this.grabbedEl = event.target;
+      this.grabbedElClone = this.grabbedEl.cloneNode(true);
+      this.grabbedElClone.classList.add('dropzone');
+      this.grabbedEl.style.width = `${this.grabbedEl.getBoundingClientRect().width}px`;
+
+      this.underDraggedEl = this.grabbedEl.parentElement;
+      this.showDropPlace(event);
+
+      this.grabbedEl.classList.add('grabbed');
+
+      this.taskBoardEl.addEventListener('mouseover', this.keepGrabbing);
+
+      this.shiftGrabbedElX = event.clientX - this.grabbedEl.getBoundingClientRect().left;
+      this.shiftGrabbedElY = event.clientY - this.grabbedEl.getBoundingClientRect().top;
+
+      this.drag(event);
+      this.taskBoardEl.addEventListener('mousemove', this.drag);
+      this.taskBoardEl.addEventListener('mouseup', this.drop);
+    }
+  }
+
+  drag(event) {
+    event.preventDefault();
+    this.grabbedEl.style.left = `${event.pageX - this.shiftGrabbedElX - this.taskBoardEl.offsetLeft}px`;
+    this.grabbedEl.style.top = `${event.pageY - this.shiftGrabbedElY - this.taskBoardEl.offsetTop}px`;
+
+    this.underDraggedEl = document.elementFromPoint(event.pageX, event.pageY);
+    this.showDropPlace(event);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  keepGrabbing(event) {
+    event.target.classList.add('keep-grabbing');
+    event.relatedTarget.classList.remove('keep-grabbing');
+  }
+
+  drop() {
+    this.taskBoardEl.removeEventListener('mousemove', this.drag);
+    this.taskBoardEl.removeEventListener('mouseover', this.keepGrabbing);
+
+    this.underDraggedEl.classList.remove('keep-grabbing');
+    this.grabbedEl.remove();
+    this.grabbedElClone.classList.remove('dropzone');
+
+    this.taskBoardEl.removeEventListener('mouseup', this.drop);
+  }
+
+  showDropPlace(dragEvent) {
+    if (this.underDraggedEl.classList.contains('task')) {
+      const underDraggedElCoords = this.underDraggedEl.getBoundingClientRect();
+      if (dragEvent.pageY < underDraggedElCoords.top + underDraggedElCoords.height / 2) {
+        this.underDraggedEl.before(this.grabbedElClone);
+      } else {
+        this.underDraggedEl.after(this.grabbedElClone);
+      }
+    }
+
+    if (this.underDraggedEl.classList.contains('task-column')) {
+      // this.underDraggedEl.append(this.grabbedElClone);
+      this.underDraggedEl.querySelector('.add-task-btn').before(this.grabbedElClone);
+    }
+
+    // if (this.underDraggedEl.classList.contains('add-task-btn')) {
+    //   // this.underDraggedEl.append(this.grabbedElClone);
+    //   this.underDraggedEl.before(this.grabbedElClone);
+    // }
+  }
+}
